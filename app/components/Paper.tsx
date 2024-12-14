7a2274762dda5adf10b45ab5f47074a94fb84b80
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-12-14 14:04:27 +0000

    feat(Paper.tsx-page.tsx): Add paper component to main page

---
+++ app/components/Paper.tsx
@@ -0,0 +1,18 @@
+export default function Paper() {
+  return (
+    <div
+      className="bg-white z-40 shadow-custom"
+      style={{
+        marginTop: '92px',
+        marginLeft: '127px',
+        width: 'calc(100vw - 254px)',
+        minHeight: 'calc((100vw - 254px) * 1.4142)',
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        opacity: 1,
+        borderRadius: '4px 4px 0 0',
+      }}
+    >
+      {/* Paper content will go here */}
+    </div>
+  );
+} 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import NavigationMain from './components/NavigationMain'
+import Paper from './components/Paper'
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-[#FFFCF9]">
+    <div className="min-h-screen bg-[#FFFCF9] pb-16">
       <NavigationMain />
+      <Paper />
     </div>
   )
 }
