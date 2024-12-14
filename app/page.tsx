20339e50e26f5dbb44be8922057835f753d533c0
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-12-14 14:21:27 +0000

    refactor(Paper.tsx-Page.tsx): Refactor Paper component into a modular component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import NavigationMain from './components/NavigationMain'
-import Paper from './components/Paper'
+import DocumentPaper from './components/DocumentPaper'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#FFFCF9] pb-16">
       <NavigationMain />
-      <Paper />
+      <DocumentPaper />
     </div>
   )
 }
